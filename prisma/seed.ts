af32b26a5c7de16137038652ccede643bfd0549b
Justin Byrd <justinbyrd7@gmail.com>
2024-03-14 13:36:01 -0500

    Add postinstall script and seed script to package.json

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,16 +4,35 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
+
+  // USERS
+  // await prisma.user.create({
+  //   data: {
+  //     username: "jwbw29",
+  //     email: "justinbyrd7@gmail.com",
+  //     password: "password",
+  //   },
+  // });
+
+  await prisma.user.update({
+    where: { id: "afdc4a20-c405-4da8-9acc-a087879f753b" },
+    data: { email: "justin.byrd7@gmail.com" },
+  });
+
   const allUsers = await prisma.user.findMany();
   console.log(allUsers);
 
-  //   await prisma.user.create({
-  //     data: {
-  //       username: "jwbw29",
-  //       email: "justinbyrd7@gmail.com",
-  //       password: "password",
-  //     },
-  //   });
+  // FAMILY
+
+  // ENNEAGRAM
+
+  // ADULT
+
+  // CHILD
+
+  // ADDRESS
+
+  // EVENT
 }
 
 main()
