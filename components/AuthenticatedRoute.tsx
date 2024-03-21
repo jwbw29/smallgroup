55dfd3aacd4a726ffcc391cf9a6ac5ed02eb77cc
Justin Byrd <justinbyrd7@gmail.com>
2024-03-21 15:52:51 -0500

    Add interface for MyUser in AuthenticatedRoute.tsx

--- components/AuthenticatedRoute.tsx
+++ components/AuthenticatedRoute.tsx
@@ -6,6 +6,17 @@ import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 // This could be in a separate file like types.ts or directly in your component file
 
+interface MyUser {
+  nickname: string;
+  name: string;
+  picture: string;
+  updated_at: string;
+  email: string;
+  email_verified: boolean;
+  sub: string;
+  "https://smallgroup.vercel.app/roles"?: string[]; // Your custom roles
+}
+
 interface AuthenticatedRouteProps {
   children: React.ReactNode;
   allowedRoles?: string[]; // Optional prop to specify allowed roles for a route
@@ -20,7 +31,9 @@ export const AuthenticatedRoute: React.FC<AuthenticatedRouteProps> = ({
   const [userRoles, setUserRoles] = useState<string[]>([]);
 
   useEffect(() => {
-    const roles = user ? user["https://smallgroup.vercel.app/roles"] || [] : [];
+    const roles = user
+      ? (user as MyUser)["https://smallgroup.vercel.app/roles"] || []
+      : [];
     setUserRoles(roles);
 
     if (!isLoading && !user) {
