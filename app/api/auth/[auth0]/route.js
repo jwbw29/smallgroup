2cc4a3fcc0c8111b24ce7079654ab42854eb8db3
Justin Byrd <justinbyrd7@gmail.com>
2024-03-21 11:11:32 -0500

    Update authentication roles for routes

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,26 +1,7 @@
 // app/api/auth/[auth0]/route.js
-import { handleAuth, handleCallback } from "@auth0/nextjs-auth0";
-import jwt from "jsonwebtoken";
+import { handleAuth } from "@auth0/nextjs-auth0";
 
-// Custom afterCallback function to enrich session with roles
-const afterCallback = async (req, res, session) => {
-  // Assuming the roles are stored in the ID Token
-  const decodedToken = jwt.decode(session.idToken);
-  const roles = decodedToken["https://smallgroup.vercel.app/roles"] || [];
-  console.log("Extracted Roles:", roles); // Log the extracted roles
-
-  session.user.roles = roles; // Add roles to the session
-
-  return session;
-};
-
-// Modify the GET handler to customize the callback handling
-export const GET = handleAuth({
-  // Override the default callback handler
-  async callback(req, res) {
-    await handleCallback(req, res, { afterCallback });
-  },
-});
+export const GET = handleAuth({});
 
 // * This creates the following routes:
 // - `/api/auth/login`
