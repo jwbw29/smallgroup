10a1a84b79c0c8313e94e20927f7be1195bd6858
Justin Byrd <justinbyrd7@gmail.com>
2024-03-21 16:28:34 -0500

    Refactor login page to use Next.js router and add redirect on user login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,23 @@
-"use client";
-
+import { useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
 import logo from "@/public/sg_logo.png";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Page() {
   const { user, error, isLoading } = useUser();
+  const router = useRouter();
+
+  console.log(user, isLoading);
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-
-  if (user) {
-    window.location.href = "/";
-  }
 
   return (
     <main className="flex flex-col min-h-screen p-2 items-center justify-center">
