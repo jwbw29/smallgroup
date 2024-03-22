5d0add466bea04a9748d3b58b889172d2c984cec
Justin Byrd <justinbyrd7@gmail.com>
2024-03-22 09:21:15 -0500

    routes are working again clean slate

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,20 @@
-import { useUser } from "@auth0/nextjs-auth0/client";
+"use client";
+
 import Image from "next/image";
 import logo from "@/public/sg_logo.png";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Page() {
   const { user, error, isLoading } = useUser();
-  const router = useRouter();
 
   console.log(user, isLoading);
 
-  useEffect(() => {
-    if (user) {
-      router.push("/");
-    }
-  }, [user, router]);
-
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    window.location.href = "/";
+  }
 
   return (
     <main className="flex flex-col min-h-screen p-2 items-center justify-center">
