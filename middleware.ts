c85a7b1446346283b395461da981849e5fccee5c
Justin Byrd <justinbyrd7@gmail.com>
2024-03-25 15:10:26 -0500

    Add getSession import to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getSession } from "@auth0/nextjs-auth0";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
