43b05f8696c8e4a0b80abb49c875172ec3790e84
Justin Byrd <justinbyrd7@gmail.com>
2024-03-26 15:01:39 -0500

    getting all roles but now need to get user specific role

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default async function Page() {
   // console.log(token.token_type);
 
   // use token to make a request to the management API
-  const rolesResponse = await fetch(audience as string, {
+  const rolesResponse = await fetch((audience as string) + "roles", {
     headers: {
       Authorization: `${token.token_type} ${token.access_token}`,
     },
@@ -43,26 +43,34 @@ export default async function Page() {
 
   const roles = await rolesResponse.json();
   console.log(roles);
-  // return <p>Data id = {roles.name}</p>;
 
   return (
-    <main className="flex flex-col min-h-screen p-2">
-      <Nav />
-      <div className="flex flex-1 testBorder justify-center">
-        <div className="flex flex-col testBorder h-96 w-3/4 gap-8 mt-28 ">
-          <h1 className="text-3xl testBorder text-center">Next Event</h1>{" "}
-          <div className="flex flex-col border-4 rounded-xl flex-1 justify-around">
-            <div className="flex flex-col items-center">
-              <h2 className="text-4xl">{"{eventName}"}</h2>
-              <h3 className=" font-bold">{"Date: {date}"}</h3>
-            </div>
-            <div className="testBorder mx-4">
-              <p>{"Who: {guys/girls/all/etc}"}</p>
-              <p>{"Where: {location}"}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+    <p>
+      Roles ={" "}
+      {roles.map((role) => {
+        return <p key={role.id}>{role.name}</p>;
+      })}
+    </p>
   );
+
+  // return (
+  //   <main className="flex flex-col min-h-screen p-2">
+  //     <Nav />
+  //     <div className="flex flex-1 testBorder justify-center">
+  //       <div className="flex flex-col testBorder h-96 w-3/4 gap-8 mt-28 ">
+  //         <h1 className="text-3xl testBorder text-center">Next Event</h1>{" "}
+  //         <div className="flex flex-col border-4 rounded-xl flex-1 justify-around">
+  //           <div className="flex flex-col items-center">
+  //             <h2 className="text-4xl">{"{eventName}"}</h2>
+  //             <h3 className=" font-bold">{"Date: {date}"}</h3>
+  //           </div>
+  //           <div className="testBorder mx-4">
+  //             <p>{"Who: {guys/girls/all/etc}"}</p>
+  //             <p>{"Where: {location}"}</p>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </main>
+  // );
 }
