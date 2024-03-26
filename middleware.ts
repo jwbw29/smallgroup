7ee5a216de912f828f386cb8c12dbf2dc56e0d7f
Justin Byrd <justinbyrd7@gmail.com>
2024-03-26 10:22:03 -0500

    Remove commented out code in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,6 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  /* //* Keep in mind
-    - Security Assumptions: Relying on the mere presence of a cookie as an indication of authentication assumes that the session it represents is valid and not expired. In many cases, especially for initial middleware checks, this is a practical and acceptable assumption. However, be aware that without validating the session (e.g., checking its signature, expiration, and so forth), there's a slight risk. Such validation is typically handled in more detail within your application, either on specific routes that require secure access or via server-side page rendering logic. */
   const token = request.cookies.get("appSession"); //
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
