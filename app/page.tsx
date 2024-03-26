f968323bd12b27abcac0ba115a055ad1b3ca5ecb
Justin Byrd <justinbyrd7@gmail.com>
2024-03-26 15:50:43 -0500

    Refactor authentication and update UI layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+// Attempt to remove getSession and use withPageAuthRequired
+
 import Nav from "@/components/nav";
+import { getSession } from "@auth0/nextjs-auth0";
 
 type Token = {
   access_token: string;
@@ -10,7 +13,6 @@ type Token = {
 export default async function Page() {
   // TODO May need to change how the url is constructed
 
-  // Replace these with your actual Auth0 client credentials
   const clientId = process.env.AUTH0_CLIENT_ID;
   const clientSecret = process.env.AUTH0_CLIENT_SECRET;
   const audience = process.env.AUTH0_AUDIENCE;
@@ -18,7 +20,7 @@ export default async function Page() {
 
   // the above is all logging correctly
 
-  // REQUEST TOKEN
+  //// REQUEST TOKEN
   const tokenResponse = await fetch(tokenUrl as string, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -31,9 +33,19 @@ export default async function Page() {
   });
 
   const token: Token = await tokenResponse.json();
-  const userId = "auth0|65fd9930f2e86684187e547f"; //fetch the userId from the session
 
-  // use token to make a request to the management API
+  const session = await getSession();
+
+  if (!session) {
+    throw new Error(`Requires authentication`);
+  }
+
+  const { user } = session;
+  const userId = user.sub;
+
+  console.log(userId);
+
+  //// use token to make a request to the management API
   const rolesResponse = await fetch(
     (audience as string) + `users/${userId}/roles`,
     {
@@ -49,51 +61,31 @@ export default async function Page() {
     description?: string;
   }
 
-  const roles = await rolesResponse.json();
+  const roles: Role[] = await rolesResponse.json();
   console.log(roles);
 
   const isMember = roles.some((role: Role) => role.name === "Member");
 
-  // Line below is to fetch all roles from the Auth0 API
-  // const rolesResponse = await fetch((audience as string) + "roles", {
-  //   headers: {
-  //     Authorization: `${token.token_type} ${token.access_token}`,
-  //   },
-  // });
-
-  // const roles = await rolesResponse.json();
-  // console.log(roles);
-
   return (
-    <div>
-      {isMember
-        ? roles.map((role: Role) => (
-            <p key={role.id}>{role.name}: Access granted</p>
-          ))
-        : roles.map((role: Role) => (
-            <p key={role.id}>{role.name}: Access Denied</p>
-          ))}
-    </div>
+    isMember && (
+      <main className="flex flex-col min-h-screen p-2">
+        <Nav />
+        <div className="flex flex-1 testBorder justify-center">
+          <div className="flex flex-col testBorder h-96 w-3/4 gap-8 mt-28 ">
+            <h1 className="text-3xl testBorder text-center">Next Event</h1>{" "}
+            <div className="flex flex-col border-4 rounded-xl flex-1 justify-around">
+              <div className="flex flex-col items-center">
+                <h2 className="text-4xl">{"{eventName}"}</h2>
+                <h3 className=" font-bold">{"Date: {date}"}</h3>
+              </div>
+              <div className="testBorder mx-4">
+                <p>{"Who: {guys/girls/all/etc}"}</p>
+                <p>{"Where: {location}"}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
   );
-
-  // return (
-  //   <main className="flex flex-col min-h-screen p-2">
-  //     <Nav />
-  //     <div className="flex flex-1 testBorder justify-center">
-  //       <div className="flex flex-col testBorder h-96 w-3/4 gap-8 mt-28 ">
-  //         <h1 className="text-3xl testBorder text-center">Next Event</h1>{" "}
-  //         <div className="flex flex-col border-4 rounded-xl flex-1 justify-around">
-  //           <div className="flex flex-col items-center">
-  //             <h2 className="text-4xl">{"{eventName}"}</h2>
-  //             <h3 className=" font-bold">{"Date: {date}"}</h3>
-  //           </div>
-  //           <div className="testBorder mx-4">
-  //             <p>{"Who: {guys/girls/all/etc}"}</p>
-  //             <p>{"Where: {location}"}</p>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </main>
-  // );
 }
