22bb6771dc639729ad4aa62fea7414a8b643349d
Justin Byrd <justinbyrd7@gmail.com>
2024-03-25 21:01:14 -0500

    Refactor authentication middleware to use token instead of isAuthenticated

--- middleware.ts
+++ middleware.ts
@@ -5,12 +5,12 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   /* //* Keep in mind
     - Security Assumptions: Relying on the mere presence of a cookie as an indication of authentication assumes that the session it represents is valid and not expired. In many cases, especially for initial middleware checks, this is a practical and acceptable assumption. However, be aware that without validating the session (e.g., checking its signature, expiration, and so forth), there's a slight risk. Such validation is typically handled in more detail within your application, either on specific routes that require secure access or via server-side page rendering logic. */
-  const isAuthenticated = request.cookies.get("appSession"); //
-  if (!isAuthenticated) {
+  const token = request.cookies.get("appSession"); //
+  if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   // TODO
-  //   else if (isAuthenticated && role === "Pending"){
+  //   else if (token && role === "Pending"){
   //      return NextResponse.redirect(new URL("/signup", request.url));
   //   }
   else {
