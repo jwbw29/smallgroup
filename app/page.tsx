ecfedf8ba746980b1d0118f02b41a8b2d5bdd574
Justin Byrd <justinbyrd7@gmail.com>
2024-03-26 14:24:30 -0500

    Refactor authentication code and remove unused environment variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import Nav from "@/components/nav";
 
+type Token = {
+  access_token: string;
+  scope: string;
+  expires_in: number;
+  token_type: string;
+};
+
 export default async function Page() {
   // TODO May need to change how the url is constructed
 
@@ -7,31 +14,55 @@ export default async function Page() {
   const clientId = process.env.AUTH0_CLIENT_ID;
   const clientSecret = process.env.AUTH0_CLIENT_SECRET;
   const audience = process.env.AUTH0_AUDIENCE;
-  const tokenUrl = process.env.AUTH0_TOKEN_URL; //TODO  this most likely isn't the right way to do it -- need to figure out how to send the token (and get a token in the first place)
+  const tokenUrl = process.env.AUTH0_TOKEN_URL;
 
-  const res = await fetch("https://dev-edet26qnl0r7r40m.us.auth0.com/api/v2/");
-  const data = await res.json();
+  // the above is all logging correctly
 
-  return <p>Data id = {data.name}</p>;
+  // REQUEST TOKEN
+  const tokenResponse = await fetch(tokenUrl as string, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      client_id: clientId as string,
+      client_secret: clientSecret as string,
+      audience: audience as string,
+      grant_type: "client_credentials",
+    }),
+  });
 
-  // return (
-  //   <main className="flex flex-col min-h-screen p-2">
-  //     <Nav />
-  //     <div className="flex flex-1 testBorder justify-center">
-  //       <div className="flex flex-col testBorder h-96 w-3/4 gap-8 mt-28 ">
-  //         <h1 className="text-3xl testBorder text-center">Next Event</h1>{" "}
-  //         <div className="flex flex-col border-4 rounded-xl flex-1 justify-around">
-  //           <div className="flex flex-col items-center">
-  //             <h2 className="text-4xl">{"{eventName}"}</h2>
-  //             <h3 className=" font-bold">{"Date: {date}"}</h3>
-  //           </div>
-  //           <div className="testBorder mx-4">
-  //             <p>{"Who: {guys/girls/all/etc}"}</p>
-  //             <p>{"Where: {location}"}</p>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </main>
-  // );
+  // TODO Change to access_token and remove the console.log
+  const token: Token = await tokenResponse.json();
+  // console.log(token.token_type);
+
+  // use token to make a request to the management API
+  const rolesResponse = await fetch(audience as string, {
+    headers: {
+      Authorization: `${token.token_type} ${token.access_token}`,
+    },
+  });
+
+  const roles = await rolesResponse.json();
+  console.log(roles);
+  // return <p>Data id = {roles.name}</p>;
+
+  return (
+    <main className="flex flex-col min-h-screen p-2">
+      <Nav />
+      <div className="flex flex-1 testBorder justify-center">
+        <div className="flex flex-col testBorder h-96 w-3/4 gap-8 mt-28 ">
+          <h1 className="text-3xl testBorder text-center">Next Event</h1>{" "}
+          <div className="flex flex-col border-4 rounded-xl flex-1 justify-around">
+            <div className="flex flex-col items-center">
+              <h2 className="text-4xl">{"{eventName}"}</h2>
+              <h3 className=" font-bold">{"Date: {date}"}</h3>
+            </div>
+            <div className="testBorder mx-4">
+              <p>{"Who: {guys/girls/all/etc}"}</p>
+              <p>{"Where: {location}"}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
