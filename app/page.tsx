c41b95cc947597e1b5ad42f82a432f68955c94b3
Justin Byrd <justinbyrd7@gmail.com>
2024-04-01 15:49:56 -0500

    Remove middleware and update authentication in roster and profile pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
 import Nav from "@/components/nav";
 import MembershipPending from "@/components/pending";
-import { getSession } from "@auth0/nextjs-auth0";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import events from "@/public/data/eventData.json";
-
-type Token = {
-  access_token: string;
-  scope: string;
-  expires_in: number;
-  token_type: string;
-};
-
-interface Role {
-  id: string;
-  name: string;
-  description?: string;
-}
 
 interface Event {
   name: string;
@@ -23,99 +10,69 @@ interface Event {
   group_type: string;
 }
 
-export default async function Page() {
-  // get today's date
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
+export default withPageAuthRequired(
+  async function Page() {
+    // get today's date
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-  // filter events to only include future ones
-  const futureEvents: Event[] = events.filter((event) => {
-    const eventDate = new Date(event.date);
-    return eventDate >= today;
-  });
+    // filter events to only include future ones
+    const futureEvents: Event[] = events.filter((event) => {
+      const eventDate = new Date(event.date);
+      return eventDate >= today;
+    });
 
-  // sort future events by date
-  futureEvents.sort((a: Event, b: Event) => {
-    const dateA = new Date(a.date).getTime();
-    const dateB = new Date(b.date).getTime();
-    return dateA - dateB;
-  });
+    // sort future events by date
+    futureEvents.sort((a: Event, b: Event) => {
+      const dateA = new Date(a.date).getTime();
+      const dateB = new Date(b.date).getTime();
+      return dateA - dateB;
+    });
 
-  // select the next event (closest to today)
-  const nextEvent: Event | null = futureEvents[0] || null;
+    // select the next event (closest to today)
+    const nextEvent: Event | null = futureEvents[0] || null;
 
-  // format the date
-  const formattedDate = new Date(nextEvent.date).toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
+    // format the date
+    const formattedDate = new Date(nextEvent.date).toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
 
-  const clientId = process.env.AUTH0_CLIENT_ID;
-  const clientSecret = process.env.AUTH0_CLIENT_SECRET;
-  const audience = process.env.AUTH0_AUDIENCE;
-  const tokenUrl = process.env.AUTH0_TOKEN_URL;
+    //Fetch user data via getSession
+    const session = await getSession();
 
-  //Fetch the token
-  const tokenResponse = await fetch(tokenUrl as string, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      client_id: clientId ?? "",
-      client_secret: clientSecret ?? "",
-      audience: audience ?? "",
-      grant_type: "client_credentials",
-    }),
-  });
-  if (!tokenResponse.ok) {
-    throw new Error("Failed to fetch the token");
-  }
-  const token: Token = await tokenResponse.json();
-  console.log("Token: ", token);
-
-  //Fetch userId via getSession
-  const session = await getSession();
-  if (!session || !session.user) {
-    throw new Error(`Requires authentication`);
-  }
-  const userId = session.user.sub;
-  console.log("UserId: ", userId);
-
-  //Use the token to fetch the roles
-  const rolesResponse = await fetch(
-    (audience as string) + `users/${userId}/roles`,
-    {
-      headers: {
-        Authorization: `${token.token_type ?? ""} ${token.access_token ?? ""}`,
-      },
+    if (!session || !session.user || !session.accessToken) {
+      throw new Error(`Requires authentication`);
     }
-  );
-  const roles: Role[] = await rolesResponse.json();
 
-  console.log("Roles: ", roles);
-  //Check if the user is a member
-  const isPending = roles?.some((role: Role) => role.name === "Pending");
+    const roles = session.user["https://smallgroup.vercel.app/roles"] || [];
+    console.log(roles);
+    //Check if the user is a member
+    const isPending = roles.some((role: string) => role === "Pending");
 
-  return isPending ? (
-    <MembershipPending />
-  ) : (
-    <main className="flex flex-col min-h-screen p-2 text-blue-950">
-      <Nav />
-      <div className="flex flex-1 justify-center">
-        <div className="flex flex-col w-3/4 max-w-[500px] gap-16 mt-28 ">
-          <h1 className="text-3xl pl-2">Next Event</h1>{" "}
-          <div className="flex flex-col shadow-custom bg-white border-4 border-blue-950 rounded-xl h-72 justify-around px-4">
-            <div className="flex flex-col items-start">
-              <h2 className="text-4xl">{nextEvent.name}</h2>
-              <h3 className="text-lg font-light">{formattedDate}</h3>
-            </div>
-            <div className="">
-              <p className="py-1 font-light">Who: {nextEvent.group_type}</p>
-              <p className="py-1 font-light">Where: {nextEvent.location}</p>
+    return isPending ? (
+      <MembershipPending />
+    ) : (
+      <main className="flex flex-col min-h-screen p-2 text-blue-950">
+        <Nav />
+        <div className="flex flex-1 justify-center">
+          <div className="flex flex-col w-3/4 max-w-[500px] gap-16 mt-28 ">
+            <h1 className="text-3xl pl-2">Next Event</h1>{" "}
+            <div className="flex flex-col shadow-custom bg-white border-4 border-blue-950 rounded-xl h-72 justify-around px-4">
+              <div className="flex flex-col items-start">
+                <h2 className="text-4xl">{nextEvent.name}</h2>
+                <h3 className="text-lg font-light">{formattedDate}</h3>
+              </div>
+              <div className="">
+                <p className="py-1 font-light">Who: {nextEvent.group_type}</p>
+                <p className="py-1 font-light">Where: {nextEvent.location}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
-}
+      </main>
+    );
+  },
+  { returnTo: "/" }
+);
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,79 +1,40 @@
 import Nav from "@/components/nav";
 import MembershipPending from "@/components/pending";
 import ProfileClient from "@/components/user";
-import { getSession } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
-type Token = {
-  access_token: string;
-  scope: string;
-  expires_in: number;
-  token_type: string;
-};
+export default withPageAuthRequired(
+  async function Page() {
+    //Fetch user data via getSession
+    const session = await getSession();
 
-interface Role {
-  id: string;
-  name: string;
-  description?: string;
-}
-
-export default async function Page() {
-  const clientId = process.env.AUTH0_CLIENT_ID;
-  const clientSecret = process.env.AUTH0_CLIENT_SECRET;
-  const audience = process.env.AUTH0_AUDIENCE;
-  const tokenUrl = process.env.AUTH0_TOKEN_URL;
-
-  //Fetch the token
-  const tokenResponse = await fetch(tokenUrl as string, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      client_id: clientId ?? "",
-      client_secret: clientSecret ?? "",
-      audience: audience ?? "",
-      grant_type: "client_credentials",
-    }),
-  });
-  if (!tokenResponse.ok) {
-    throw new Error("Failed to fetch the token");
-  }
-  const token: Token = await tokenResponse.json();
-
-  //Fetch userId via getSession
-  const session = await getSession();
-  if (!session || !session.user) {
-    throw new Error(`Requires authentication`);
-  }
-  const userId = session.user.sub;
-
-  //Use the token to fetch the roles
-  const rolesResponse = await fetch(
-    (audience as string) + `users/${userId}/roles`,
-    {
-      headers: {
-        Authorization: `${token.token_type ?? ""} ${token.access_token ?? ""}`,
-      },
+    if (!session || !session.user || !session.accessToken) {
+      throw new Error(`Requires authentication`);
     }
-  );
-  const roles: Role[] = await rolesResponse.json();
-  //Check if the user is a member
-  const isPending = roles?.some((role: Role) => role.name === "Pending");
 
-  return isPending ? (
-    <MembershipPending />
-  ) : (
-    <main className="flex flex-col min-h-screen p-2">
-      <Nav />
-      <div className="flex flex-col flex-1 items-center gap-8">
-        <h1 className="text-3xl text-center my-6">Profile</h1>{" "}
-        <div className="flex flex-col h-fit w-3/4 gap-8 my-6">
-          <ProfileClient />
+    const roles = session.user["https://smallgroup.vercel.app/roles"] || [];
+    console.log(roles);
+    //Check if the user is a member
+    const isPending = roles.some((role: string) => role === "Pending");
+
+    return isPending ? (
+      <MembershipPending />
+    ) : (
+      <main className="flex flex-col min-h-screen p-2">
+        <Nav />
+        <div className="flex flex-col flex-1 items-center gap-8">
+          <h1 className="text-3xl text-center my-6">Profile</h1>{" "}
+          <div className="flex flex-col h-fit w-3/4 gap-8 my-6">
+            <ProfileClient />
+          </div>
+          <div className="flex w-3/4 justify-end">
+            <a href="/api/auth/logout">
+              <button className="border p-2 px-6 rounded-lg">Logout</button>
+            </a>
+          </div>
         </div>
-        <div className="flex w-3/4 justify-end">
-          <a href="/api/auth/logout">
-            <button className="border p-2 px-6 rounded-lg">Logout</button>
-          </a>
-        </div>
-      </div>
-    </main>
-  );
-}
+      </main>
+    );
+  },
+  { returnTo: "/profile" }
+);
