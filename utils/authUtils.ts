8701796f58dcef7719aabed502c2196e0eedcfe0
Justin Byrd <justinbyrd7@gmail.com>
2024-04-02 09:05:44 -0500

    removed consoleLog

--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -2,7 +2,6 @@ import { getSession } from "@auth0/nextjs-auth0";
 
 export async function getUserSessionAndRoles() {
   const session = await getSession();
-  console.log(session);
 
   if (!session || !session.user) {
     throw new Error(`Requires authentication`);
