bd637c74fcfa299076da605bce77f14e7f46409d
Justin Byrd <justinbyrd7@gmail.com>
2024-04-03 09:07:09 -0500

    removed unnecessary notes

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -23,19 +23,3 @@ export default withPageAuthRequired(
   },
   { returnTo: "/schedule" }
 );
-
-/** // **NOTES
- * should default to the current semester
- * and the route by default should be /schedule/{current semester}
- * ***** OR ******
- * we get rid of the fall23 and spring24 routes
- * and whatever is chosen in the drop down just displays different data
- * fall23 = 08/01/2023 - 12/31/2023
- * spring24 = 01/01/2024 - 05/31/2024
- * fall24 = 08/01/2024 - 12/31/2024
- * etc
- *
- *
- *
- *
- */
