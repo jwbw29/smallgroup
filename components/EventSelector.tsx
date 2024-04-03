6686f6340dc5a48b6c6c1c96d2be06a508917dc1
Justin Byrd <justinbyrd7@gmail.com>
2024-04-02 21:54:40 -0500

    done with schedule page

--- components/EventSelector.tsx
+++ components/EventSelector.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import eventData from "@/public/data/eventData.json";
 import { EventDetails } from "@/components/event";
-import { Dropdown } from "flowbite-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const getCurrentSemester = () => {
   const today = new Date();
@@ -14,7 +20,6 @@ const getCurrentSemester = () => {
   } else if (month >= 1 && month <= 5) {
     return `Spring ${year}`;
   } else {
-    // Adjust based on your semester system; maybe Summer or a previous semester
     return `Summer ${year}`;
   }
 };
@@ -22,13 +27,11 @@ const getCurrentSemester = () => {
 const EventSelector = () => {
   const currentSemester = getCurrentSemester();
   const [selectedSemester, setSelectedSemester] = useState(currentSemester);
-  const [isExpanded, setIsExpanded] = useState(false);
 
-  // Assuming the eventData format allows this kind of filtering directly
   const filteredEvents = eventData.filter((event) => {
     const semesterYear = selectedSemester.split(" ");
     const semester = semesterYear[0];
-    const year = semesterYear[1]; // Converts "'23" to "2023"
+    const year = semesterYear[1];
     return event.year === year && event.semester === semester;
   });
 
@@ -36,38 +39,37 @@ const EventSelector = () => {
     setSelectedSemester(semester);
   };
 
-  const toggle = () => {
-    setIsExpanded(!isExpanded);
-  };
-
   return (
     <div className="flex flex-col">
-      <div className="testBorder flex justify-center my-6 ">
-        {/* -------A stab at custom dropdown--------- */}
-        <div className="testBorder flex h-fit w-3/4 justify-end">
-          <button onClick={toggle} className="primaryButton">
-            {selectedSemester}
-          </button>
-          {/* TODO make this float */}
-          {isExpanded && (
-            <div className="bg-white rounded border px-2 py-1 my-2 w-full ">
-              <li className="listItem">{"Spring '24"}</li>
-              <li className="listItem">{"Fall '23"}</li>
-            </div>
-          )}
+      <div className="flex justify-center my-6 ">
+        <div className="flex h-fit w-3/4 justify-end">
+          <Select
+            onValueChange={setSelectedSemester}
+            defaultValue={selectedSemester}
+          >
+            {" "}
+            <SelectTrigger className="bg-blue-950 rounded-md text-white w-[9rem] min-w-fit">
+              <SelectValue placeholder="Choose a Semester" />
+            </SelectTrigger>
+            <SelectContent className="bg-white">
+              <SelectItem
+                value="Spring 2024"
+                onClick={() => handleDropdownSelect("Spring 2024")}
+              >
+                {"Spring '24"}
+              </SelectItem>
+              <SelectItem
+                value="Fall 2023"
+                onClick={() => handleDropdownSelect("Fall 2023")}
+              >
+                {"Fall '23"}
+              </SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-        {/* -------A stab at custom dropdown--------- */}
-        {/* <Dropdown label={selectedSemester} dismissOnClick={true}>
-          <Dropdown.Item onClick={() => handleDropdownSelect("Spring 2024")}>
-            {"Spring '24"}
-          </Dropdown.Item>
-          <Dropdown.Item onClick={() => handleDropdownSelect("Fall 2023")}>
-            {"Fall '23"}
-          </Dropdown.Item>
-        </Dropdown> */}
       </div>
-      <div className="testBorder flex flex-col flex-1 items-center ">
-        <div className="testBorder flex flex-col h-fit w-3/4 gap-8 my-6">
+      <div className="flex flex-col flex-1 items-center ">
+        <div className="flex flex-col h-fit w-3/4 gap-8 my-6">
           {filteredEvents.map((event, i) => (
             <EventDetails key={i} event={event} />
           ))}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,49 @@ const config: Config = {
       colors: {
         primary: "#4895EF",
         secondary: "#4CC9F0",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
