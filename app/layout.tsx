541542a591271c419b55c0cabfd62ddc7a34f7ce
Justin Byrd <justinbyrd7@gmail.com>
2024-04-03 22:01:43 -0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { Analytics } from "@vercel/analytics/react";
 
 const roboto = Roboto_Mono({
   weight: ["100", "200", "300", "400", "400", "500", "600", "700"],
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        <body className={roboto.className}>{children}</body>
+        <body className={roboto.className}>
+          {children}
+          <Analytics />
+        </body>
       </UserProvider>
     </html>
   );
