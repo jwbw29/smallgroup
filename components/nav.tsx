e352ebea29c2dbc2eef1c8c1e8ce5375aeb1e838
Justin Byrd <justinbyrd7@gmail.com>
2024-04-04 08:33:56 -0500

    added profile pic to nav bar

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,20 +3,24 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const links = [
   { name: "Home", href: "/" },
   { name: "Schedule", href: "/schedule" },
   { name: "Roster", href: "/roster" },
-  { name: "Profile", href: "/profile" },
+  // { name: "Profile", href: "/profile" },
 ];
 
 //TODO Might need to change this back to Page() if there are errors
 export default function Navigation() {
   const pathname = usePathname();
+  const { user } = useUser();
+  const profilePic = user?.picture;
 
   return (
-    <nav className=" flex justify-around gap-2 lg:justify-end lg:gap-12 border-b-2 border-b-white p-4 mx-4">
+    <nav className=" flex justify-center gap-4 lg:justify-end lg:gap-12 border-b-2 border-b-white p-4 mx-4">
       <div className=" flex gap-2 lg:justify-end lg:gap-12">
         {links.map((link) => {
           return (
@@ -31,7 +35,14 @@ export default function Navigation() {
             </Link>
           );
         })}
+        {/* //// PROFILE IMAGE HERE */}
       </div>
+      <Link href="/profile" className="self-center">
+        <Avatar>
+          <AvatarImage src={profilePic ?? ""} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </Link>
     </nav>
   );
 }
