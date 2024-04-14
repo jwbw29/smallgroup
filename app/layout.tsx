b2b5a985ae38d63de79281cd8a0ddc7d935e36c2
Justin Byrd <justinbyrd7@gmail.com>
2024-04-14 14:03:23 -0500

    Add SpeedInsights component to layout.tsx and update package.json and yarn.lock

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const roboto = Roboto_Mono({
   weight: ["100", "200", "300", "400", "400", "500", "600", "700"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <body className={roboto.className}>
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </UserProvider>
     </html>
